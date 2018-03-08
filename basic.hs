42470fa1a4c097860bb59fd810750a947700ad3a
Liam <liam.jb1@gmail.com>
2018-03-09 10:29:34 +1300

    Some more guard and pattern examples

--- basic.hs
+++ basic.hs
@@ -14,3 +14,12 @@ max x y = if x > y then x else y
 absg :: Int -> Int
 absg x  | x >= 0 = x
         | x < 0 = -x
+
+-- Factorial with guards and explicit types
+factg :: Int -> Int
+factg x | x == 0 = 1
+        | otherwise = x * fact(x-1)
+
+-- Factorial pattern matching
+factp 0 = 1
+factp x = x * factp(x-1)
