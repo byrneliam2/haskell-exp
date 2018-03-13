bab6c9c0faafa11356b5fe5b5994a7071b9e5655
Liam <liam.jb1@gmail.com>
2018-03-14 09:43:17 +1300

    Rough exercises, probably doesn't compile yet

--- haskell/exercises.hs
+++ haskell/exercises.hs
@@ -1,1 +1,18 @@
 -- Exercises as suggested in lectures
+
+-- indexOf function, directly from slides
+pos :: Eq a => a -> [a] -> Int
+pos x l = pos' x 1 l
+pos' :: Eq a => a -> Int -> [a] -> Int
+pos' _ _ [] = 0
+pos' x k (y : _) | x == y = k
+pos' x k (_ : ys) = pos' x (k+1) ys
+
+-- Fibonacci without accumulators
+fibb x | x < 2 = 1
+       | otherwise = fibb(x-1) + fibb(x-2)
+
+-- Fibonacci with accumulators
+fiba x = fiba' x 0
+         where fiba' 2 f = 1
+               fiba' x f = fiba' (x-1) + fiba' (x-2) 
