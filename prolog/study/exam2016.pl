200175c4fe2f7c282f7f011e97666a31dbb33e7b
Liam <liam.jb1@gmail.com>
2018-06-21 10:57:54 +1200

    Yet more Prolog

--- prolog/study/exam2016.pl
+++ prolog/study/exam2016.pl
@@ -1,5 +1,6 @@
 %  ================================== 2016 ===================================
 
+% 4.
 plays(bale, batman).
 plays(affleck, batman).
 plays(reeve, superman).
@@ -7,11 +8,28 @@ plays(downey, ironman).
 plays(downey, sherlock).
 plays(cumberbatch, sherlock).
 
-% 1. a)
+% 4. a)
 bale(Y) :- plays(bale, X), plays(Y, X).
 
-% 1. b)
+% 4. b)
 pairs(X, Y) :- plays(X, Z), plays(Y, Z), not(X == Y).
 
-% 1. d)
+% 4. d)
 get(A, C) :- findall(X, plays(A, X), C).
+
+% 6.
+controls(government, fbi).
+controls(fbi, police).
+controls(government, cia).
+controls(police, people).
+controls(sarah, tom).
+agency(X) :- controls(government, X). % agency must be controlled by govt
+
+% 6. a)
+force(X) :- agency(Y), controls(Y, X).
+
+% 6. b)
+hasControlOver(X, Y) :- controls(X, Y) ; controls(X, Z), controls(Z, Y).
+
+% 6. c)
+limitedControl(X) :- not(agency(X)), not(X == government).
