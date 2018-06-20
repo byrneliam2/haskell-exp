ce5166654e84f3e03fcf279d984d9ccff85e9741
Liam <liam.jb1@gmail.com>
2018-06-20 13:29:24 +1200

    Trees n' stuff

--- haskell/misc/exam.hs
+++ haskell/misc/exam.hs
@@ -90,6 +90,29 @@ count3 x l = length [i | (i, n) <- zip [0..] l, n == x]
 count4 :: Eq a => a -> [a] -> Int
 count4 x l = foldr (\x y -> if x then y + 1 else y) 0 $ map (== x) l
 
+-- 2. a)
+data Tree a = Node2 a [Tree a] | Leaf a
+
+findLeaves :: Tree a -> Int
+findLeaves (Leaf _) = 1
+findLeaves (Node2 _ t) = sum $ map findLeaves t
+
+-- 2. b)
+data Tree2 a c = Node3 a Int [Tree2 a Int] | Leaf2 a Int
+
+-- 2. c)
+leastCost :: Tree2 a c -> ([a], Int)
+leastCost t = leastCost' t []
+
+leastCost' :: Tree2 a c -> [a] -> ([a], Int)
+leastCost' (Leaf2 a c) p = (p ++ [a], c)
+leastCost' (Node3 a c t) p = getLeast $ map (\x -> leastCost' x (p ++ [a])) t
+
+getLeast (p:ps) = getLeast' ps p
+getLeast' [] p = p
+getLeast' ((p, c):ps) (px, cx) | c < cx = getLeast' ps (p, c)
+                               | otherwise = getLeast' ps (px, cx)
+
 -- ================================== 2015 ===================================
 
 -- 1. b) ii.
