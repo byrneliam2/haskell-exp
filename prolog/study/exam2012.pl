dedaa33812e3c5659b45225e0fc7c60d6a4b0866
Liam <liam.jb1@gmail.com>
2018-06-25 14:31:10 +1200

    2012 exam

--- prolog/study/exam2012.pl
+++ prolog/study/exam2012.pl
@@ -17,3 +17,21 @@ q5b :- grandfather(X, Z).
 
 % 5. c)
 q5c :- findall(X, mother(M, X), L).
+
+% 7.
+orbits(earth, sun).
+orbits(mars, sun).
+orbits(gliese581d, gliese581).
+orbits(moon, earth).
+orbits(phobos, mars).
+planet(X) :- orbits(X, sun).
+
+% 7. a)
+satellite(X) :- orbits(X, Y), planet(Y).
+
+% 7. b)
+movesAround(X, Y) :- orbits(X, Y).
+movesAround(X, Y) :- orbits(X, Z), movesAround(Z, Y).
+
+% 7. c)
+bigCelestialBody(X) :- not(satellite(X)).
